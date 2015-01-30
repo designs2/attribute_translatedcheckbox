84a23da31acee4a9b7cbf05885b4b9fc1495c1b1
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-01-30 08:44:05 +0100

    Drop sorting and flags.

--- src/MetaModels/Attribute/TranslatedCheckbox/TranslatedCheckbox.php
+++ src/MetaModels/Attribute/TranslatedCheckbox/TranslatedCheckbox.php
@@ -44,7 +44,6 @@ class TranslatedCheckbox extends TranslatedReference
             'check_publish',
             'filterable',
             'searchable',
-            'sortable',
             'submitOnChange'
         ));
     }
