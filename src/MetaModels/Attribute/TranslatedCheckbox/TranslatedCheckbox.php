designs2/attribute_translatedcheckbox

commit 05384f595801f32ad46774f157951a086ebb9c73

Fixup: Do not use fallback language values for published checkboxes.

src/MetaModels/Attribute/TranslatedCheckbox/TranslatedCheckbox.php
@@ -74,10 +74,14 @@ class TranslatedCheckbox extends TranslatedReference
     {
         $arrReturn = parent::getTranslatedDataFor($arrIds, $strLangCode);
 
-        // Per definition: all values that are not contained are defaulting to false in the fallback language.
-        if (($strLangCode == $this->getMetaModel()->getFallbackLanguage()) && (count($arrReturn) < count($arrIds))) {
-            foreach (array_diff(array_keys($arrReturn), $arrIds) as $intId) {
-                $arrReturn[$intId] = $this->widgetToValue(false, $intId);
+        if (count($arrReturn) < count($arrIds)) {
+            // Per definition:
+            // - all values that are not contained are defaulting to false in the fallback language.
+            // - all values in published not contained are defaulting to false.
+            if ($this->isPublishedField() || ($strLangCode == $this->getMetaModel()->getFallbackLanguage())) {
+                foreach (array_diff($arrIds, array_keys($arrReturn)) as $intId) {
+                    $arrReturn[$intId] = $this->widgetToValue(false, $intId);
+                }
             }
         }
 

commit 1ebe8d65e199ee8783b76f5422b7b3601c9cc815

Values of new items can be saved again
The Problem was introduced in 05384f595801f32ad46774f157951a086ebb9c73
as now getTranslatedValuesFor() will always return values.
This behaviour is now changed in the following manner:
- All values will now be saved when retrieved but not contained in
database.
- To prevent recursion, a locking flag has been introduced.
- The check if fixing of values is needed has been extracted into a new
method.

src/MetaModels/Attribute/TranslatedCheckbox/TranslatedCheckbox.php
@@ -26,6 +26,13 @@ use MetaModels\Attribute\TranslatedReference;
 class TranslatedCheckbox extends TranslatedReference
 {
     /**
+     * Private flag to lock filling of missing values in getTranslatedDataFor.
+     *
+     * @var bool
+     */
+    private $doNotFixValues = false;
+
+    /**
      * Check if the attribute is a published field.
      *
      * @return bool
@@ -74,17 +81,51 @@ class TranslatedCheckbox extends TranslatedReference
     {
         $arrReturn = parent::getTranslatedDataFor($arrIds, $strLangCode);
 
-        if (count($arrReturn) < count($arrIds)) {
-            // Per definition:
-            // - all values that are not contained are defaulting to false in the fallback language.
-            // - all values in published not contained are defaulting to false.
-            if ($this->isPublishedField() || ($strLangCode == $this->getMetaModel()->getFallbackLanguage())) {
-                foreach (array_diff($arrIds, array_keys($arrReturn)) as $intId) {
-                    $arrReturn[$intId] = $this->widgetToValue(false, $intId);
-                }
+        // Per definition:
+        // - all values that are not contained are defaulting to false in the fallback language.
+        // - all values in published not contained are defaulting to false.
+        if ($this->isFixingOfValuesNeeded($arrReturn, $arrIds, $strLangCode)) {
+            // We have to lock the retrieval to prevent endless recursion.
+            $this->doNotFixValues = true;
+
+            $fixedValues = array();
+            foreach (array_diff($arrIds, array_keys($arrReturn)) as $itemId) {
+                $arrReturn[$itemId]   = $this->widgetToValue(false, $itemId);
+                $fixedValues[$itemId] = $arrReturn[$itemId];
             }
+
+            if (count($fixedValues)) {
+                $this->setTranslatedDataFor($fixedValues, $strLangCode);
+            }
+
+            // Unlock the retrieval again as we have fixed the values in the database.
+            $this->doNotFixValues = false;
         }
 
         return $arrReturn;
+    }
+
+    /**
+     * Check if the passed values need to be fixed (filled up) with missing ids.
+     *
+     * @param array    $values   The retrieved values.
+     *
+     * @param string[] $idList   The list of ids.
+     *
+     * @param string   $langCode The language code.
+     *
+     * @return bool
+     */
+    private function isFixingOfValuesNeeded($values, $idList, $langCode)
+    {
+        if ($this->doNotFixValues) {
+            return false;
+        }
+
+        if (count($values) == count($idList)) {
+            return false;
+        }
+
+        return ($this->isPublishedField() || ($langCode == $this->getMetaModel()->getFallbackLanguage()));
     }
 }